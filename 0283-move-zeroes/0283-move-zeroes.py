c6454e31a71dd13ed74024390b64ff98c93060b1
SANJANA KUMARESAN <sanjanakumaresan6@gmail.com>
2026-03-13 20:08:11 +0530

    Time: 15 ms (17.41%), Space: 13.7 MB (14.48%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index] = nums[i] 
+                index += 1
+        while index < len(nums):
+            nums[index] = 0
+            index += 1
+        
